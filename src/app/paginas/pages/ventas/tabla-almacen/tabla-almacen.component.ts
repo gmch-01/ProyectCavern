6a91620b93d5524b5ccbe830c8afbc9f95ceeddb
Manuel Cuevas <manuelhinojosagc@gmail.com>
2023-01-06 01:40:09 -0400

    servicios almacen productos

--- src/app/paginas/pages/ventas/tabla-almacen/tabla-almacen.component.ts
+++ src/app/paginas/pages/ventas/tabla-almacen/tabla-almacen.component.ts
@@ -6,10 +6,9 @@ import { MatSort, Sort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { FormularioAlmacenComponent } from '../formulario-almacen/formulario-almacen.component';
 
-let codigo: string;
-let name: string;
-let nombre: string;
-let id: number;
+import { AlmacenFinService } from '../../../../services/almacenfin.service';
+import { AlmacenFin } from '../../../../models/AlmacenFin';
+import { VentasComponent } from '../ventas.component'
 /**
  * @title Table with sorting
  */
@@ -23,31 +22,18 @@ export class TablaAlmacenComponent implements OnInit {
 
   displayedColumns: string[] = [
     'id_det_productos',
-    'fecha_registro',
+    'fecha_vencimiento',
     'cantidad',
     'producto',
-    'encargado'
+    'accion'
   ];
   //dataSource = new MatTableDataSource(ELEMENT_DATA);
 
-  datos: Articulo[] = [];
+  datos: any;
   dataSource: any;
 
-  codigo = '';
-  name = '';
-  nombre = '';
-  id = '';
-  apellido = '';
 
   @ViewChild(MatPaginator, { static: true }) paginator!: MatPaginator;
-
-  constructor(
-    private _liveAnnouncer: LiveAnnouncer,
-    public dialog: MatDialog
-  ) {}
-  
-  appName: string = 'Tabla';
-  @ViewChild(MatSort) sort!: MatSort;
 
   ngAfterViewInit() {
     this.dataSource.sort = this.sort;
@@ -61,43 +47,36 @@ export class TablaAlmacenComponent implements OnInit {
     }
   }
 
-  ngOnInit(): void {
-   /*  for (let x = 1; x <= 100; x++)
-      this.datos.push(
-        new Articulo(
-          x,
-          `usuario ${x}`,
-          `apellido ${x}`,
-        
-          Math.trunc(Math.random() * 1000)
-        
-        )
-      ); */
+  constructor(
+    private _liveAnnouncer: LiveAnnouncer,
+    public dialog: MatDialog,
+    private almacenFinService: AlmacenFinService
+  ) {
+    this.almacenFinService.getAlmacenfin().subscribe(x => {
+      this.datos = this.datos;
+      console.log(this.datos)
+    })
 
-    this.dataSource = new MatTableDataSource<Articulo>(this.datos);
-    this.dataSource.paginator = this.paginator;
   }
+
+  appName: string = 'Tabla';
+  @ViewChild(MatSort) sort!: MatSort;
+
+  ngOnInit(): void {
+    this.datos = JSON.parse(localStorage.getItem("listaAlmacen")!)
+    console.log("llego ", this.datos);
+  }
+
 
   openDialog(): void {
     let dialogRef = this.dialog.open(FormularioAlmacenComponent, {
       width: '400px',
-      data: { name: this.codigo, animal: this.codigo },
+      data: {},
     });
 
     dialogRef.afterClosed().subscribe((result) => {
       console.log('The dialog was closed');
-      this.codigo = result;
     });
   }
 }
 
-export class Articulo {
-  constructor(
-    public id_det_productos: number,
-    public feha_registro: string,
-    public cantidad: number,
-    public producto: string,
-    public encargado:string,
- 
-  ) {}
-}
--- src/app/paginas/pages/ventas/ventas.component.ts
+++ src/app/paginas/pages/ventas/ventas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AlmacenFinService } from '../../../services/almacenfin.service';
 
 @Component({
   selector: 'app-ventas',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VentasComponent implements OnInit {
 
-  constructor() { }
+  almacenfin: any;
+  constructor(private AlmacenFinService: AlmacenFinService) { }
 
   ngOnInit(): void {
+    console.log("entro");
+    this.getAlmacen();
+  }
+
+  getAlmacen() {
+    this.AlmacenFinService.getAlmacenfin().subscribe(
+      res => {
+        this.almacenfin = res;
+        localStorage.setItem("listaAlmacen", JSON.stringify(this.almacenfin));
+        console.log(this.almacenfin);
+      },
+
+      err => console.error(err)
+    )
   }
 
 }
